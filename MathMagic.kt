dee878c66b0909f2e30459e640541c53139a8c0e
Sumaya Alqadhy <sumayaalqadhy@Sumayas-MacBook-Pro.local>
2024-08-30 11:13:40 -0700

    math magic code

---
+++ MathMagic.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+  val userName = "Sumaya"
+  var magicNum : Int
+
+  magicNum = 3 * 5
+
+  magicNum += 3
+
+  magicNum *= 2
+
+  magicNum += 0
+
+  magicNum -= 6
+
+  println("$userName's age is $magicNum.")
+
+}
